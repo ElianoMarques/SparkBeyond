49393a5225275edf57fd760d3feac1f7a9bb2afd
zinman <guy.zinman@gmail.com>
2015-03-18 16:48:45 +0200

    write to stderr

--- R/example.R
+++ R/example.R
@@ -30,7 +30,7 @@ run_SB_examples <- function() {
       return (1)
     }
   }, error = function(e) {
-    print (e$message, stderr())
+    write (e$message, stderr())
     return (0)
   })
   return (0)
