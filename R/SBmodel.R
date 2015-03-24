de86614ee3f923cfd27516cd2bf65a1bf7cb67fc
zinman <guy.zinman@gmail.com>
2015-03-24 18:34:27 +0200

    Add feature search only modeling Split the test module into simple functions

--- R/SBmodel.R
+++ R/SBmodel.R
@@ -4,13 +4,15 @@
 SBmodel = setRefClass("SBmodel",
   fields = list(
     artifact_loc = "character",
-    server_port = "numeric"
+    server_port = "numeric",
+    modelBuilt = "logical"
   ),
   methods = list(
     initialize = function(artifact_loc, server_port = 9000) {
       "initializes a model using a string provided as \\code{loc}."
       artifact_loc<<- artifact_loc
       server_port <<- server_port
+      modelBuilt <<- FALSE
     },
 
     predict = function(dataPath, outputPath) {
@@ -82,8 +84,10 @@ SBmodel = setRefClass("SBmodel",
       "\\code{report_name} should be one of the following: confusionMatrix, confusionMatrix_normalized, extractor, features, field, function, InputSchema, modelComparison, roc_best, roc_CV"
       validReports = c("confusionMatrix", "confusionMatrix_normalized", "extractor", "features", "field",
                        "function", "InputSchema", "modelComparison", "roc_best", "roc_CV")
-      if (is.na(report_name) || ! reportName %in% validReports ) stop("Report name is not valid")
-      if (showInIDE && (reportName == "roc_best" || reportName == "roc_CV")) print ("Graphs cannot be shown in the IDE and will be displayed in the external browser")
+      if (is.na(report_name) || ! report_name %in% validReports ) stop("Report name is not valid")
+      reportsThatRequireModel = c("confusionMatrix", "confusionMatrix_normalized", "modelComparison", "roc_best", "roc_CV")
+      if (!modelBuilt && report_name %in% reportsThatRequireModel) stop("This report requires model building")
+      if (showInIDE && (report_name == "roc_best" || report_name == "roc_CV")) print ("Graphs cannot be shown in the IDE and will be displayed in the external browser")
       #verify model created, check if classification, file exists
       htmlSource <- paste(artifact_loc,"/reports/", report_name, ".html",sep="")
       viewer <- getOption("viewer")
@@ -97,10 +101,6 @@ SBmodel = setRefClass("SBmodel",
         utils::browseURL(htmlSource)
     },
 
-    showConfusionMatrixReport = function(normalized = FALSE, showInIDE = TRUE){
-      "Shows a confusion matrix of a model in the IDE viewer on in the web browser."
-      showReport(if (normalized) "confusionMatrix_normalized", "confusionMatrix",showInIDE)
-    },
     showModelExtractorsReport = function(showInIDE = TRUE){
       "Shows extractors used by a model in the IDE viewer on in the web browser."
       showReport("extractor",showInIDE)
@@ -121,6 +121,12 @@ SBmodel = setRefClass("SBmodel",
       "Shows the input schema a model in the IDE viewer on in the web browser."
       showReport("InputSchema",showInIDE)
     },
+
+    #require model methods
+    showConfusionMatrixReport = function(normalized = FALSE, showInIDE = TRUE){ #verify that this was a classification problem
+      "Shows a confusion matrix of a model in the IDE viewer on in the web browser."
+      showReport(if (normalized) "confusionMatrix_normalized" else "confusionMatrix",showInIDE)
+    },
     showModelComparisonReport = function(showInIDE = TRUE){
       "Shows cross validation of various algorithms tested to create a model in the IDE viewer on in the web browser."
       showReport("modelComparison",showInIDE)
@@ -134,13 +140,15 @@ SBmodel = setRefClass("SBmodel",
       showReport("roc_CV",FALSE) #problematic to show in internal browser non local resources
     }
 
-    # add all reports + showReport
-    # add features json
+
+    # add featureSearch with ZeroR as method - return enriched
     # serialize / deserialize
     # non blocking
+    # add features json
     # lift, regression plots
-    # feature clusters
+    # feature clusters report
     # featurePlot
+    # add S3 methods of print, predict
 
   )
 );
@@ -148,8 +156,7 @@ SBmodel = setRefClass("SBmodel",
 
 
 
-
-
+# S3 functions (De facto constructors of SBmodel)
 
 
 #' Run SparkBeyond feature enrichment and learning process.
@@ -163,14 +170,14 @@ SBmodel = setRefClass("SBmodel",
 #' @param algorithmsWhiteList: Optional. A list of strings that represents the set of algorithms to run. NA by default
 #' @param hints: Optional. A list of strings that reprents a set of hints that will be used to guide the feature search. NA by default.
 #' @param useGraph: Optional. A boolean indicating whether the knowledge graph should be used. FALSE by default.
-#' @param globalFeatureIterations: Optional. An integer of how many features should be created by the SB engine. 300 by default.
+#' @param maxFeaturesCount: Optional. An integer of how many features should be created by the SB engine. 300 by default.
 #' @param evaluationMetric: Optional. A string representing the evaluation metric. Should be either "AUC", "PREC", or "RMSE". "PREC" by default.
 #' @param scoreOnTestSet: Optional. A boolean representing whether scoring should be provided for the test set. FALSE by default.
 #' @param crossValidation: Optional. Integer value representing how many cross validation splits should be used. 5 by default.
 #' @param server_port Optional. the port to be accessed in the SparkBeyond API server. 9000 by default.
-#' @return model path of the prediction on \code{trainingFilePath}.
+#' @return SBmodel object the encapsulate the prediction.
 #' @examples
-#' modelRes = SBlearn("titanic", titanic_train_filename, "survived")
+#' model = SBlearn("titanic", titanic_train_filename, "survived")
 SBlearn <- function(sessionName, trainingFilePath, target,
                     testFilePath = NA,
                     trainTestSplitRatio = 0.8,
@@ -179,10 +186,11 @@ SBlearn <- function(sessionName, trainingFilePath, target,
                     algorithmsWhiteList = NA, #list available algorithms
                     hints = NA,
                     useGraph = FALSE,
-                    globalFeatureIterations = 300, #make it a list
+                    maxFeaturesCount = 300, #make it a list
                     evaluationMetric = "PREC", #add all options
                     scoreOnTestSet = FALSE,
                     crossValidation = 5,
+                    runBlocking = TRUE,
                     server_port = 9000){
 
   url <- paste("http://127.0.0.1:",server_port,"/rapi/learn", sep="")
@@ -196,7 +204,7 @@ SBlearn <- function(sessionName, trainingFilePath, target,
                 algorithmsWhiteList = algorithmsWhiteList,
                 hints = hints,
                 useGraph = useGraph,
-                globalFeatureIterations = globalFeatureIterations,
+                globalFeatureIterations = maxFeaturesCount,
                 evaluationMetric = evaluationMetric,
                 scoreOnTestSet = scoreOnTestSet,
                 crossValidation = crossValidation
@@ -212,6 +220,8 @@ SBlearn <- function(sessionName, trainingFilePath, target,
     print(res)
     stop(res)
   }
+
+  print(paste("Artifact location was created at:", res$artifactPath))
 
   serverResponded = FALSE
   i = 0
@@ -239,5 +249,47 @@ SBlearn <- function(sessionName, trainingFilePath, target,
     }
   }
   m = SBmodel(res$artifactPath, server_port)
+  model$modelBuilt = TRUE
   return(m)
 }
+
+
+
+#' Run SparkBeyond feature enrichment and learning process.
+#' @param sessionName String of the session name.
+#' @param trainingFilePath String of the path to the file to be trained on.
+#' @param target String of the column name of in the training file that conatins the target of the prediction.
+#' @param weightColumn: Optional. String of the name of of one of the column that indicate a weighting that is assigned to each example. NA by default.
+#' @param maxDepth: Optional. Integer < 8 which represent the maximun number of transformations allowed during the feature search phase. Increasing this value should be considered with cautious as the feature search phase is exponential. 2 by default.
+#' @param hints: Optional. A list of strings that reprents a set of hints that will be used to guide the feature search. NA by default.
+#' @param useGraph: Optional. A boolean indicating whether the knowledge graph should be used. FALSE by default.
+#' @param maxFeaturesCount: Optional. An integer of how many features should be created by the SB engine. 300 by default.
+#' @param server_port Optional. the port to be accessed in the SparkBeyond API server. 9000 by default.
+#' @return SBmodel object that encapsulate the feature search result.
+#' @examples
+#' model = SBfeatureSearchOnly("titanic", titanic_train_filename, "survived")
+SBfeatureSearchOnly <- function(sessionName, trainingFilePath, target,
+                    weightColumn = NA,
+                    maxDepth = 2,
+                    hints = NA,
+                    useGraph = FALSE,
+                    maxFeaturesCount = 300, #make it a list
+                    runBlocking = TRUE,
+                    server_port = 9000){
+
+  params <-list("sessionName" = sessionName,
+                "trainingFilePath" = trainingFilePath,
+                target = target,
+                weightColumn = weightColumn,
+                maxDepth = maxDepth,
+                algorithmsWhiteList = list("ZeroR"),
+                hints = hints,
+                useGraph = useGraph,
+                maxFeaturesCount = maxFeaturesCount,
+                runBlocking = runBlocking,
+                server_port = server_port)
+  model = do.call(SBlearn,c(params))
+  model$modelBuilt = FALSE
+  model
+}
+
