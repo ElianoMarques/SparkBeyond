d1749aee8d3f89f176d40c196e2acb9222963dd3
zinman <guy.zinman@gmail.com>
2015-03-26 15:59:18 +0200

    changed doucmentation file

--- R/documentation.R
+++ R/documentation.R
@@ -1,18 +1,21 @@
-static_help = function(pkg, links = tools::findHTMLlinks()) {
-  pkgRdDB = tools:::fetchRdDB(file.path(find.package(pkg), 'help', pkg))
-  force(links); topics = names(pkgRdDB)
-  for (p in topics) {
-    tools::Rd2HTML(pkgRdDB[[p]], paste('html/',p, '.html', sep = ''),
-                   package = pkg, Links = links, no_links = is.null(links))
-  }
-}
+# static_help = function(pkg, links = tools::findHTMLlinks()) {
+#   pkgRdDB = tools:::fetchRdDB(file.path(find.package(pkg), 'help', pkg))
+#   force(links); topics = names(pkgRdDB)
+#   for (p in topics) {
+#     tools::Rd2HTML(pkgRdDB[[p]], paste('html/',p, '.html', sep = ''),
+#                    package = pkg, Links = links, no_links = is.null(links))
+#   }
+# }
+
 #dir.create(file.path('./', 'html'), showWarnings = FALSE)
 #static_help('SBadapter')
 
-#dir.create(file.path('./', 'html'), showWarnings = FALSE)
-# setwd("./html")
-# library(knitr)
-# setwd("..")
+createHelp = function() {
+  dir.create(file.path('./', 'html'), showWarnings = FALSE)
+  setwd("./html")
+  library(knitr)
+  setwd("..")
+}
 
 # knit_rd("SBadapter")
 
