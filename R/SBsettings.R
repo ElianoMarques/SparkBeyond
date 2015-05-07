63bf463ffa5c41abd9351534677c5219641d95e4
zinman <guy.zinman@gmail.com>
2015-05-07 17:37:14 +0300

    re-adding backslash at the end of the IO folder

--- R/SBsettings.R
+++ R/SBsettings.R
@@ -55,7 +55,7 @@ setSBserverIOfolder = function(folder){
     curPrefix
   } else ""
   folder = paste0(prefix,gsub("\\\\","/",folder))
- # if (substr(folder, nchar(folder), nchar(folder)) != "/") folder = paste0(folder, "/")
+  if (substr(folder, nchar(folder), nchar(folder)) != "/") folder = paste0(folder, "/")
   #if (substr(folder, nchar(folder)-1, nchar(folder)) != "//") folder = paste0(folder, "/")
   if (is.null(folder) || folder == "") {stop("folder location is empty")}
   if (!file.exists(folder)) {
