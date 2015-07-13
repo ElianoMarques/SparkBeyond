8cd3b6f697524bc9ac96580aa0dcf74d8d4ec05e
zinman <guy.zinman@gmail.com>
2015-07-13 13:03:11 +0300

    updates to examples - removed direct calls to titanic functions added general getData()

--- R/Session.R
+++ R/Session.R
@@ -5,11 +5,11 @@ library(methods) #to support RScript
 #' @field modelBuilt Indication for whether only a feature search was performed or a full model was created.
 #' @examples
 #' # Learning example
-#' session = learn("titanic", getTitanicData(train = TRUE), target="survived",algorithmsWhiteList = list("xgboostClassifier"),  scoreOnTestSet = TRUE, useCachedFeatures=TRUE)
-#' #session = featureSearch("titanic", getTitanicData(train = TRUE), target="survived")
-#' enriched = session$enrich(getTitanicData(train = FALSE), featureCount = 10)
+#' session = learn("titanic", getData("titanic_train"), target="survived",algorithmsWhiteList = list("xgboostClassifier"),  scoreOnTestSet = TRUE, useCachedFeatures=TRUE)
+#' #Do Not Run: session = featureSearch("titanic", getData("titanic_train"), target="survived")
+#' enriched = session$enrich(getData("titanic_train"), featureCount = 10)
 #' colnames(enriched)
-#' predicted = session$predict(getTitanicData(train = FALSE))
+#' predicted = session$predict(getData("titanic_test"))
 #' colnames(predicted)
 #' predicted[1:5,c("survived_predicted", "probability_0", "probability_1")]
 #' eval = session$evaluate()
