2b5d63a99806cd7d420a12f0bba64e879a57716e
zinman <guy.zinman@gmail.com>
2015-10-29 12:10:17 +0200

    removing body printout in createPackage

--- R/Session.R
+++ R/Session.R
@@ -333,7 +333,7 @@ Session = setRefClass("Session",
         print(paste("Calling:", url))
 
         body = rjson::toJSON(params)
-        print(body)
+       # print(body)
         res = httr::POST(url, body = body, httr::content_type_json())
         res <- jsonlite::fromJSON(txt=httr::content(res, as="text"),simplifyDataFrame=TRUE)
         finalRes = if (is.null(res$error) && !is.null(res$result) && res$result == "OK"){
