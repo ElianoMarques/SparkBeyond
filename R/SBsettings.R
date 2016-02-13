79ddf9f552ff7454d54914a84b6f17652443da4f
zinman <guy.zinman@gmail.com>
2016-02-13 21:44:53 +0200

    removing from the help previous folder settings functions

--- R/SBsettings.R
+++ R/SBsettings.R
@@ -1,23 +1,17 @@
 # Global variables
 
-# Server Host
-# ===========
-#' A function to set the SparkBeyond server host.
-#' @param host new host URL.
 setSBserverHost = function(host = "http://127.0.0.1") {
   assign("SBhost", host, envir = globalenv())
   #print(paste("Setting server host to:", SBhost))
   return(SBhost)
 }
 
-#' A function to get the SparkBeyond server host.
 getSBserverHost = function() {
   host = if (exists("SBhost")) SBhost else setSBserverHost()
   return(host)
 }
 
 getSBserverDomain = function() {
-
 	port = getSBserverPort()
 	if (substr(getSBserverHost(), 1, 5) == "https" || grepl(":", substr(getSBserverHost(), 6, nchar(getSBserverHost()))) || nchar(port) == 0)
 		getSBserverHost()
@@ -25,38 +19,27 @@ getSBserverDomain = function() {
 		paste0(getSBserverHost(),":", port)
 }
 
-#' A function to print the SparkBeyond server host.
 printSBserverHost = function() {
   host = getSBserverHost()
   print(paste("Server host is:", host))
 }
 
-# Server Port
-# ===========
-#' A function to set the SparkBeyond server port.
-#' @param port new port.
 setSBserverPort = function(port = "9000") {
   assign("SBport", port, envir = globalenv())
   #print(paste("Setting server port to:", SBport))
   return(SBport)
 }
 
-#' A function to get the SparkBeyond server port.
 getSBserverPort = function() {
   port = if (exists("SBport")) SBport else setSBserverPort()
   return(port)
 }
 
-#' A function to print the SparkBeyond server port.
 printSBserverPort = function() {
   port = getSBserverPort()
   print(paste("Server port is:", port))
 }
 
-# Server IO Folder
-# ======================
-#' A function to set the SparkBeyond server I/O folder.
-#' @param port new port.
 setSBserverIOfolder = function(folder) {
 	if (is.null(folder)) {
 		assign("IOfolder", folder, envir = globalenv())
@@ -90,7 +73,6 @@ setSBserverIOfolder = function(folder) {
 	}
 }
 
-#' A function to get the SparkBeyond server IO folder.
 getSBserverIOfolder = function() {
   folder = if (exists("IOfolder")) IOfolder else {
     print("Server IO folder was not defined. Please define folder using setSBserverIOfolder")
@@ -99,20 +81,17 @@ getSBserverIOfolder = function() {
   return(folder)
 }
 
-#' A function to print the SparkBeyond server IO folder.
 printSBserverIOfolder = function() {
   folder = getSBserverIOfolder()
   print(paste("Server IO folder is:", folder))
 }
 
-#' Print all current settings.
 printSettings = function() {
   printSBserverIOfolder()
   printSBserverHost()
   printSBserverPort()
 }
 
-#' Save settings to the current folder.
 saveSettings = function() {
   SB_IOfolder = getSBserverIOfolder()
   SB_HOST = getSBserverHost()
@@ -122,7 +101,6 @@ saveSettings = function() {
   save(SB_IOfolder, SB_HOST, SB_PORT, file = "settings.RData")
 }
 
-#' Load settings from the current folder.
 loadSettings = function() {
   if (file.exists("settings.RData")) {
     load("settings.RData")
@@ -137,8 +115,6 @@ loadSettings = function() {
 
 # Server functions:
 
-#' A function to restart server
-#' @return The response from the server.
 restartServer = function() {
   stop("This function has been deprecated, as now jobs are handled by a queue")  #switch to be used for killing a job in the current queue - as a method on Session?
 # 	url <- paste0(getSBserverHost(),":",getSBserverPort(),"/rapi/die")
@@ -165,7 +141,6 @@ restartServer = function() {
 }
 
 
-#Deprecated
 isLatestVersion = function() {
 	print ("This function has been deprecated, as now jobs are handled by a queue")
 	TRUE
